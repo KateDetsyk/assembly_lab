5f46055bc6bc88e8801782805355a4bd99c4466e
KateD <36380026+KateDetsyk@users.noreply.github.com>
2019-12-04 19:17:05 +0200

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,21 +1,58 @@
 format elf64
-public _start
+public main
 
 include "bubble_sort.inc"
 
+extrn printf
+	
 section '.data' writable
 	array db 5, 4, 3, 2, 1
 	array_size equ 5
+	fmt db "%d", 0xA, 0
 	
-section '.text' executable
-_start:
+section '.text' writable
+main:
 	mov rax, array
 	mov rbx, array_size
 	call bubble_sort
-	call exit
 	
-section '.exit' executable
-exit:
-	mov rax, 1 ;1 --- exit
-	mov rbx, 0 ; 0 --- return
-	int 0x80
+	call print_array
+
+	xor rax, rax ; return zero	
+	ret
+	
+section '.print_array' executable
+; function to print array using printf from c, but i get segmentation fault there,
+; so it's print only first number in array
+;input rax = array
+;rbx = array_size
+print_array:
+	push rsi
+	push rdi
+	push rax
+	push rbx
+	push rcx 
+	mov rcx, rax
+	xor rax, rax
+	.next_iter:
+		cmp rbx, 0
+		je .close
+		mov al, [rcx]
+	
+		mov rdi, fmt
+	    mov rsi, rax
+		xor rax, rax
+		call printf
+
+		inc rcx
+		dec rbx
+		jmp .next_iter
+	.close:
+		pop rcx
+		pop rbx
+		pop rax
+		pop rdi
+		pop rsi
+
+		ret
+	
